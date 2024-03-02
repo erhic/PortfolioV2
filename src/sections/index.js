bb6d110791afa10cc658dfbe1ecf937d679d06af
erhic <ericngugi24@gmail.com>
2024-03-03 00:06:25 +0300

    refactor: create a file to manage export of section components to app components

---
+++ src/sections/index.js
@@ -0,0 +1,8 @@
+import Hero from "./Hero";
+import Aboutme from "./Aboutme";
+import Myservices from "./Myservices";
+import Projects from "./Projects";
+import Reviews from "./Reviews";
+import Footer from "./Footer";
+
+export { Hero, Aboutme, Myservices, Projects, Reviews, Footer };
