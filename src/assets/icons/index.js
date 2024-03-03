ef2bbfe3133373f75a34b1e624baf2519d9e7e48
erhic <ericngugi24@gmail.com>
2024-03-03 23:49:49 +0300

    feat: add constants and assets ,resources

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -9,9 +9,30 @@ import download from "./download.svg";
 import github from "./github.svg";
 import linkedin from "./linkedin.svg";
 import video from "./video.svg";
+import app from "./app.svg";
+import avatar from "./avatar.svg";
+import briefcase from "./briefcase.svg";
+import caledar from "./caledar.svg";
+import design from "./design.svg";
+import develop from "./develop.svg";
+import email from "./email.svg";
+import figma from "./figma.svg";
+import graduate from "./graduate.svg";
+import notion from "./notion.svg";
+import phoneno from "./phoneno.svg";
+import vscode from "./vscode.svg";
+
 export {
+  app,
   arrowRight,
+  avatar,
+  briefcase,
+  caledar,
+  design,
+  develop,
+  email,
   facebook,
+  figma,
   copyrightSign,
   hamburger,
   instagram,
@@ -19,6 +40,10 @@ export {
   contact,
   download,
   github,
+  graduate,
   linkedin,
   video,
+  notion,
+  phoneno,
+  vscode,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,32 @@
+import {
+  contact,
+  download,
+  github,
+  instagram,
+  linkedin,
+  video,
+} from "../assets/icons";
+
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#myprojects", label: "My Projects" },
   { href: "#contact", label: "Contact" },
+];
+
+export const aboutMe = [
+  { label: "Personal Info" },
+  { label: "Qualifications" },
+  { label: "Skills" },
+];
+
+export const socialIcons = [
+  { socialicon: video, altname: "Video icon" },
+  { socialicon: linkedin, altname: "Linkedin icon" },
+  { socialicon: github, altname: "Github icon" },
+  { socialicon: instagram, altname: "Instagram icon" },
+];
+
+export const heroButtons = [
+  { label: "Contact Me", btnIcon: contact },
+  { label: "Download CV", btnIcon: download },
 ];
