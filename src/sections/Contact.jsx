3b3178be7392bf1085f8789e003200d49ba4049d
erhic <ericngugi24@gmail.com>
2024-03-06 00:52:19 +0300

    feat: create contact section and footer

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,16 @@
+import Button from "../Components/Button"
+
+
+const Contact = () => {
+  return (
+    <div className="flex flex-row justify-center pb-16 pt-6">
+      <p className=" flex justify-center py-10 font-palanquin font-bold text-2xl">Prepared to turn your ideas into reality? I amm here to help</p>
+      <div className="absolute mt-16 pt-16 ">
+        <Button label="Contact Me" buttonIcon='' />
+      </div>
+
+    </div >
+  )
+}
+
+export default Contact
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,9 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex justify-center p-16 bg-blue-950">
+      <p className="font-pallidium leading-8  text-lg text-white">Copyright 2024  |  Eric Ngugi | All rights reserved . </p>
+    </div>
   )
 }
 
