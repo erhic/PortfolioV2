d881cf2960a9b1ef80ffd694c81345c392d1930c
erhic <ericngugi24@gmail.com>
2024-03-06 00:51:27 +0300

    feat: create review section

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -1,7 +1,32 @@
+import { hero } from "../assets/images"
+import { reviews } from "../constants"
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <>
+      <h2 className="flex justify-center font-montserrat text-2xl font-bold text-slate-gray leading-8 pb-16">Reviews</h2>
+
+      <div className="flex justify-center xl:flex-row sm:flex-col gap-5">
+        {
+          reviews.map((item) => (
+            <div key={item.comment} className="border border-red-200 rounded-lg max-w-[400px]">
+              <div className="flex flex-row justify-center gap-4 items-center pt-6">
+                <img src={hero} width={90} alt="Profile Image" className="overflow-hidden rounded-full" />
+                <div>
+                  <p className="font-semibold text-xl text-slate-gray">{item.person}</p>
+                  <p className="text-base leading-8">{item.work}</p>
+                </div>
+
+              </div>
+              <div>
+                <p className="px-8 pt-4 pb-9 leading-normal font-palanquin text-slate-gray text-lg">{item.comment}</p>
+              </div>
+
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
