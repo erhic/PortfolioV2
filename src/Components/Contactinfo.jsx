8e00326420886987058162b76363bedf471a6e41
erhic <ericngugi24@gmail.com>
2024-03-13 17:10:27 +0300

    update contact section ,add link

---
+++ src/Components/Contactinfo.jsx
@@ -0,0 +1,62 @@
+import Button from "./Button"
+
+const Contactinfo = ({ view }) => {
+  return (
+
+    <div className={`pt-16 ${view}`}>
+      <div className="px-10 py-10">
+        <h2 className="font-montserrat text-lg flex justify-center font-bold text-blue-900 py-5">Get in Touch</h2>
+        <form action="" className="flex max-lg:flex-col px-14 py-9 items-start font-montserrat max-lg:gap-5 hover:border-blue-900 border justify-between rounded-lg">
+
+          <div className="flex flex-col gap-5 text-slate-gray ">
+            <div className="flex ">
+              <label htmlFor="">Name: </label>
+              <input type="text" placeholder=" Eric Ngugi" className="outline-none mx-2" />
+            </div>
+            <div className="flex ">
+              <label htmlFor="">Phone: </label>
+              <input type="text" placeholder="254790688795" className=" outline-none mx-2" />
+            </div>
+
+            <div>
+              <label htmlFor="">Email: </label>
+              <input type="text" placeholder="ericngugi24@gmail.com" className="outline-none mx-3" />
+            </div>
+            <div>
+              <label htmlFor="">Address: </label>
+              <input type="text" placeholder="Nairobi ,Kenya" className="outline-none mx-3" />
+            </div>
+            <div>
+              <label htmlFor="">Address: </label>
+              <input type="text" placeholder="Nairobi ,Kenya" className="outline-none mx-3" />
+            </div>
+
+          </div>
+          <div className="flex flex-col gap-10 border-l pl-4">
+            <div className="flex ">
+              <label htmlFor="">Name </label>
+              <input type="text" placeholder="" className="border-b hover:border-blue-900 outline-none mx-2" />
+            </div>
+            <div className="flex ">
+              <label htmlFor="">Phone </label>
+              <input type="text" placeholder="" className="border-b hover:border-blue-900 file:outline-none mx-2" />
+            </div>
+
+            <div>
+              <label htmlFor="">Email </label>
+              <input type="text" placeholder="" className="border-b hover:border-blue-900 outline-none mx-3" />
+            </div>
+            <textarea placeholder="Type your message here" name="" id="" cols="30" rows="3" className="outline-blue-900 p-5 border hover:border-blue-900"></textarea>
+            <button className="max-w-[30px] ">
+              <Button label="Submit" buttonIcon="" />
+            </button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+
+  )
+}
+
+export default Contactinfo
