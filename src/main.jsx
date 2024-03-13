e60567ac621dda1724d3c1dfe5f444ace1077b9a
erhic <ericngugi24@gmail.com>
2024-03-13 17:07:23 +0300

    feat: set the path for every element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AllProjects from './Components/AllProjects.jsx'
+import Contact from './sections/Contact.jsx'
+import Home from './sections/Home.jsx'
+import Contactinfo from './Components/Contactinfo.jsx'
 
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/', element: <Home />
+      },
+      {
+        path: '/allprojects', element: <AllProjects />
+      },
+      {
+        path: '/contact', element: <Contactinfo />
+      }
+    ],
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <RouterProvider router={router} />
 )
+
