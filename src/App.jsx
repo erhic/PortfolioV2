2ea4ae4796349c8abb642f95bb5dc866d3eb74b9
erhic <ericngugi24@gmail.com>
2024-03-13 17:06:40 +0300

    feat: set up router using router dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
+
+import { Outlet } from "react-router-dom"
 import Nav from "./Components/Nav"
-import { Hero, Aboutme, Myservices, Projects, Reviews, Footer } from "./sections"
-import Contact from "./sections/Contact"
+import { Footer } from "./sections"
+
 
 export default function App() {
   return (
     <main>
       <section><Nav /> </section>
-      <section className="xl:padding-l px-16 wide:padding-r padding-b"><Hero /> </section>
-      <section className="padding"><Aboutme /> </section>
-      <section className="padding-x py-10"><Myservices /> </section>
-      <section className="padding"><Projects /> </section>
-      <section className="padding bg-pale-blue"><Reviews /> </section>
-      <section className="padding-x sm:py-32 py-16 w-full"><Contact /></section>
+      <Outlet />
       <section className=""><Footer /> </section>
     </main>
   )
